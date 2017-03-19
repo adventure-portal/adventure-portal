8df0b954af5f1ece2eda02a7d1acf2d7389b2d1a
Nathan <nathancy@hawaii.edu>
2017-03-18 17:33:58 -1000

    Mockup for Landing Page, Implemented Navigation bar, login/not login state

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -8,3 +8,18 @@ import './home-page.html';
 
 import './list-stuff-page.html';
 import './list-stuff-page.js';
+
+import './login-page.html';
+import './login-page.js';
+
+import './dashboard-page.html';
+import './dashboard-page.js';
+
+import './add-activity-page.js';
+import './add-activity-page.html';
+
+import './adventures-page.js';
+import './adventures-page.html';
+
+import './my-profile.js';
+import './my-profile.html';
