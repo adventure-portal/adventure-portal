6045e33ea092d97f1ad354fbeb7ab55b8d9dfe09
Nathan <nathancy@hawaii.edu>
2017-04-11 22:18:42 -1000

    Modify settings.json to allow deploy

--- app/imports/ui/layouts/if-admin-logged-in.js
+++ app/imports/ui/layouts/if-admin-logged-in.js
@@ -1,18 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-/* eslint-disable object-shorthand */
 
 Template.If_Admin_Logged_In.helpers({
-  /**
-   * @returns {*} True if Meteor is in the process of logging in.
-   */
+
+   // @returns {*} True if Meteor is in the process of logging in.
+   //
   authInProcess: function authInProcess() {
     return Meteor.loggingIn();
   },
-  /**
-   * @returns {boolean} True if there is a logged in user.
-   */
+   //@returns {boolean} True if there is a logged in user.
+
   canShow: function canShow() {
     return !!Meteor.user();
   },
