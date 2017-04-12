9b0dc171bdd30a27b1cdd2e33a75d84e8b889713
dylannakahodo <dylannakahodo@gmail.com>
2017-04-11 14:40:12 -1000

    Add UsersCollection

--- app/imports/api/users/UsersCollection.js
+++ app/imports/api/users/UsersCollection.js
@@ -1,35 +1,90 @@
-import { Mongo } from 'meteor/mongo';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import BaseCollection from '/imports/api/base/BaseCollection';
 import { check } from 'meteor/check';
 import { Meteor } from 'meteor/meteor';
 
-/* eslint-disable object-shorthand */
-
-export const Users = new Mongo.Collection('Users');
+/** @module Users */
 
 /**
- * Create the schema for Users
+ * Users provides information for a user.
+ * @extends module:Base~BaseCollection
  */
-export const UserSchema = new SimpleSchema({
-  name: {
-    label: 'Name',
-    type: String,
-    optional: false,
-    max: 20,
-    autoform: {
-      group: 'Stuff',
-      placeholder: 'Bicycle',
-    },
-  },
-  quantity: {
-    label: 'Quantity',
-    type: Number,
-    optional: false,
-    autoform: {
-      group: 'Stuff',
-      placeholder: '3',
-    },
-  },
-});
+class UsersCollection extends BaseCollection {
 
-Users.attachSchema(UserSchema);
+  /**
+   * Creates the Users collection.
+   */
+  constructor() {
+    super('User', new SimpleSchema({
+      username: { type: String },
+      firstName: { type: String, optional: true },
+      lastName: { type: String, optional: true },
+      email: { type: String, optional: true },
+      bio: { type: String, optional: true },
+      interests: { type: [String], optional: true },
+      currentActivities: { type: [String], optional: true },
+      picture: { type: SimpleSchema.RegEx.Url, optional: true },
+    }));
+  }
+
+  /**
+   * Defines a new User.
+   * @example
+   * Users.define({ firstName: 'John',
+   *                lastName: 'Doe',
+   *                username: 'jdoe',
+   *                email: 'jdoe@example.com',
+   *                bio: 'Born and raised on Oahu. Love hiking and surfing.',
+   *                interests: ['Surfing', 'Hiking', 'Camping'],
+   *                currentActivities: ['Kuliouou Ridge Trail', 'Diamond Head Hike', 'Surfing at White Plains'],
+   *                picture: 'http://johndoe.com/profile.png'});
+   * @param { Object } description Object with required key username.
+   * The rest of the keys are optional.
+   * Username must be unique for all users. It should be the UH email account.
+   * Interests is an array of defined interest names.
+   * @throws { Meteor.Error } If a user with the supplied username already exists.
+   * @returns The newly created docID.
+   */
+  define({ firstName = '', lastName = '', username, email = '', bio = '', interests, currentActivities,
+      picture = '' }) {
+    const checkPattern = {
+      firstName: String,
+      lastName: String,
+      username: String,
+      email: String,
+      bio: String,
+      picture: String,
+    };
+    check({ firstName, lastName, username, email, bio, picture }, checkPattern);
+
+    if (this.find({ username }).count() > 0) {
+      throw new Meteor.Error(`${username} is previously defined.`);
+    }
+    return this._collection.insert({ firstName, lastName, username, email, bio, interests,
+      currentActivities, picture });
+  }
+
+  /**
+   * Returns an object representing the User docID in a format acceptable to define().
+   * @param docID The docID of a User.
+   * @returns { Object }
+   */
+  dumpOne(docId) {
+    const doc = this.findDoc(docId);
+    const firstName = doc.firstName;
+    const lastName = doc.lastName;
+    const username = doc.username;
+    const email = doc.email;
+    const bio = doc.bio;
+    const interests = doc.interests;
+    const currentActivities = doc.currentActivities;
+    const picture = doc.picture;
+    return { firstName, lastName, username, email, bio, interests, currentActivities, picture };
+  }
+}
+
+/**
+ * Provides the single instance of this class to all other entities.
+ */
+
+export const Users = new UsersCollection();
