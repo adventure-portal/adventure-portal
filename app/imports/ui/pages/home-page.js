7527cf96b3b20a0db22bd92084993e19e6bc33ae
Nathan Lam <nathancy@hawaii.edu>
2017-04-25 10:30:14 -1000

    Font clarity fix

---
+++ app/imports/ui/pages/home-page.js
@@ -0,0 +1,35 @@
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+/* eslint-disable no-console */
+
+Template.Home_Page.events({
+  /**
+   * Handle the click on the login link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .ui.huge.button.login': function casLogin(event) {
+    event.preventDefault();
+    const callback = function loginCallback(error) {
+      if (error) {
+        console.log(error);
+      }
+    };
+    Meteor.loginWithCas(callback);
+    return false;
+  },
+
+  /**
+   * Handle the click on the logout link.
+   * @param event The click event.
+   * @returns {boolean} False.
+   */
+  'click .ui.huge.button.logout': function casLogout(event) {
+    event.preventDefault();
+    Meteor.logout();
+    FlowRouter.go('/');
+    return false;
+  },
+});
