c51d960fd4dade23cc3a8e41e5149e2fdc9d928b
dylannakahodo <dylannakahodo@gmail.com>
2017-05-02 10:12:34 -1000

    on click event for heart on find adventures page

--- app/imports/ui/pages/adventures-page.js
+++ app/imports/ui/pages/adventures-page.js
@@ -2,6 +2,8 @@ import { Template } from 'meteor/templating';
 import { Activities } from '/imports/api/activity/ActivityCollection';
 import { Interests } from '/imports/api/interests/InterestsCollection';
 
+/* eslint-disable object-shorthand, no-unused-vars */
+
 Template.Adventures_Page.onRendered(function onRendered() {
   this.$('.ui.dropdown').dropdown();
 });
@@ -15,5 +17,14 @@ Template.Adventures_Page.helpers({
   activities() {
     return Activities.find({}, { sort: { title: 1 } });
   },
+});
+
+Template.Adventures_Page.events({
+  /* Favorite event on clicking heart */
+  'click .heart': function (event) {
+    event.preventDefault();
+    console.log('Clicked Heart');
+  },
+
 });
 
