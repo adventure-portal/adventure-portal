adf115dfc99aca19775f852f9f4e41d80bf124d0
dylannakahodo <dylannakahodo@gmail.com>
2017-05-03 00:02:56 -1000

    saves activity to user favorites

--- app/imports/ui/pages/adventures-page.js
+++ app/imports/ui/pages/adventures-page.js
@@ -1,6 +1,9 @@
 import { Template } from 'meteor/templating';
 import { Activities } from '/imports/api/activity/ActivityCollection';
 import { Interests } from '/imports/api/interests/InterestsCollection';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Users } from '/imports/api/users/UsersCollection';
+import { _ } from 'meteor/underscore';
 
 /* eslint-disable object-shorthand, no-unused-vars */
 
@@ -22,9 +25,18 @@ Template.Adventures_Page.helpers({
 Template.Adventures_Page.events({
   /* Favorite event on clicking heart */
   'click .heart': function (event) {
+    const userProfile = Users.findDoc(FlowRouter.getParam('username'));
+    const activityId = this.activity._id;
     event.preventDefault();
-    console.log('Clicked Heart');
+    if (_.contains(userProfile.pinnedActivities, activityId)) {
+      Users.update({ _id: userProfile._id }, { $pull: { pinnedActivities: activityId } });
+    }
+    else {
+      if (userProfile.pinnedActivities === null) {
+        Users.update({ _id: userProfile._id }, { $set: { pinnedActivities: [] } });
+      }
+      Users.update({ _id: userProfile._id }, { $push: { pinnedActivities: activityId } });
+    }
   },
-
 });
 
--- app/imports/ui/pages/dashboard-page.js
+++ app/imports/ui/pages/dashboard-page.js
@@ -1,8 +1,10 @@
 import { Template } from 'meteor/templating';
 import { Activities } from '/imports/api/activity/ActivityCollection';
+import { Users } from '/imports/api/users/UsersCollection';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { _ } from 'meteor/underscore';
 
 /* eslint-disable object-shorthand, no-unused-vars */
-
 
 Template.Dashboard_Page.onCreated(function onCreated() {
   this.subscribe(Activities.getPublicationName());
@@ -21,8 +23,18 @@ Template.Dashboard_Page.helpers({
 Template.Dashboard_Page.events({
   /* Favorite event on clicking heart */
   'click .heart': function (event) {
+    const userProfile = Users.findDoc(FlowRouter.getParam('username'));
+    const activityId = this.activity._id;
     event.preventDefault();
-    console.log('Clicked Heart');
+    if (_.contains(userProfile.pinnedActivities, activityId)) {
+      Users.update({ _id: userProfile._id }, { $pull: { pinnedActivities: activityId } });
+    }
+    else {
+      if (userProfile.pinnedActivities === null) {
+        Users.update({ _id: userProfile._id }, { $set: { pinnedActivities: [] } });
+      }
+      Users.update({ _id: userProfile._id }, { $push: { pinnedActivities: activityId } });
+    }
   },
 
 });
