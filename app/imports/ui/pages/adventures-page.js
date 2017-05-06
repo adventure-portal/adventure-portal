08899de3421fbfa1415072a6a58dd0e95c5854d8
Nathan Lam <nathancy@hawaii.edu>
2017-05-05 20:57:32 -1000

    Cleanup

--- app/imports/ui/pages/adventures-page.js
+++ app/imports/ui/pages/adventures-page.js
@@ -20,8 +20,6 @@ Template.Adventures_Page.onRendered(function onRendered() {
 
 Template.Adventures_Page.helpers({
   activities() {
-
-
      // Initialize selectedInterests to all of them if messageFlags is undefined.
      if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
      Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
@@ -32,12 +30,6 @@ Template.Adventures_Page.helpers({
 
     console.log(Activities.find().fetch());
      return _.filter(allActivities, activity => _.intersection(activity.interests, selectedInterests).length > 0);
-
-
-/*
-    console.log(Activities.find().fetch());
-    return Activities.find({}, { sort: { title: 1 } });
-  */
   },
   interests() {
     return _.map(Interests.findAll(),
@@ -58,55 +50,3 @@ Template.Adventures_Page.events({
     console.log("cllicked");
   },
 });
-
-/*
-
-
- import { Template } from 'meteor/templating';
- import { ReactiveDict } from 'meteor/reactive-dict';
- import { _ } from 'meteor/underscore';
- import { Profiles } from '/imports/api/profile/ProfileCollection';
- import { Interests } from '/imports/api/interest/InterestCollection';
-
- const selectedInterestsKey = 'selectedInterests';
-
- Template.Filter_Page.onCreated(function onCreated() {
- this.subscribe(Interests.getPublicationName());
- this.subscribe(Profiles.getPublicationName());
- this.messageFlags = new ReactiveDict();
- this.messageFlags.set(selectedInterestsKey, undefined);
- });
-
- Template.Filter_Page.helpers({
- profiles() {
- // Initialize selectedInterests to all of them if messageFlags is undefined.
- if (!Template.instance().messageFlags.get(selectedInterestsKey)) {
- Template.instance().messageFlags.set(selectedInterestsKey, _.map(Interests.findAll(), interest => interest.name));
- }
- // Find all profiles with the currently selected interests.
- const allProfiles = Profiles.findAll();
- const selectedInterests = Template.instance().messageFlags.get(selectedInterestsKey);
- return _.filter(allProfiles, profile => _.intersection(profile.interests, selectedInterests).length > 0);
- },
-
- interests() {
- return _.map(Interests.findAll(),
- function makeInterestObject(interest) {
- return {
- label: interest.name,
- selected: _.contains(Template.instance().messageFlags.get(selectedInterestsKey), interest.name),
- };
- });
- },
- });
-
- Template.Filter_Page.events({
- 'submit .filter-data-form'(event, instance) {
- event.preventDefault();
- const selectedOptions = _.filter(event.target.Interests.selectedOptions, (option) => option.selected);
- instance.messageFlags.set(selectedInterestsKey, _.map(selectedOptions, (option) => option.value));
- },
- });
-
-
- */
